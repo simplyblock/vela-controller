015b74354ed893a7d63f00c6507fc18c61a76523
Manohar Reddy <manohar@simplyblock.io>
2025-09-22 12:48:51 +0200

    update default deployment host to localhost

--- src/deployment/settings.py
+++ src/deployment/settings.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="vela_", case_sensitive=False)
 
     deployment_namespace_prefix: str = "vela"
-    deployment_host: str = "_"
+    deployment_host: str = "localhost"
 
 
 settings = Settings()  # type: ignore[call-arg]
