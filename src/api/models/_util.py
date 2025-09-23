24148f0ab3f84081f86f76ab02659d46d74d80f4
Christoph Engelbert (noctarius) <me@noctarius.com>
2025-09-23 14:32:23 +0200

    Exchanged slugify for ulid reference slugs for uniqueness across all projects, which is required for subdomain names

--- src/api/models/_util.py
+++ src/api/models/_util.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 
-from pydantic import AfterValidator, StringConstraints
-from slugify import slugify
+import ulid
+from pydantic import StringConstraints
 
 from ..._util import dbstr
 
@@ -18,19 +18,12 @@ Slug = Annotated[
 
 
 def update_slug(mapper, connection, target):  # noqa
-    target.slug = slugify(target.name, max_length=50)
-
-
-def _validate_sluggable(string: str):
-    if len(slugify(string, max_length=50)) == 0:
-        raise ValueError("Derived slug is empty")
-    return string
+    target.slug = str(ulid.ULID())
 
 
 Name = Annotated[
     dbstr,
     StringConstraints(
         min_length=1,
-    ),
-    AfterValidator(_validate_sluggable),
+    )
 ]
