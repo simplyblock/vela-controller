9bf87491f35ec0e13f3119b72e17467d19c46e95
Max Schettler <max@simplyblock.io>
2025-09-29 12:50:04 +0200

    Introduce base model
    This reduces the amount of code duplication

--- src/api/models/_util.py
+++ src/api/models/_util.py
@@ -1,7 +1,11 @@
 from typing import Annotated
 
 from pydantic import AfterValidator, StringConstraints
+from pydantic import Field as PDField
 from slugify import slugify
+from sqlalchemy import BigInteger
+from sqlmodel import Field as SQLField
+from sqlmodel import SQLModel
 
 from ..._util import dbstr
 
@@ -23,3 +27,26 @@ Name = Annotated[
     ),
     AfterValidator(_validate_sluggable),
 ]
+
+
+Identifier = Annotated[int, PDField(ge=-(2**63), lt=2**63)]
+_DatabaseIdentifier = BigInteger
+
+
+class Model(SQLModel):
+    id: Identifier | None = SQLField(primary_key=True, sa_type=_DatabaseIdentifier)
+
+    # This would ideally be a classmethod, but initialization order prevents that
+    @staticmethod
+    def foreign_key_field(table_name, *, nullable=False, **kwargs):
+        return SQLField(
+            default=None if nullable else ...,
+            foreign_key=f"{table_name}.id",
+            sa_type=_DatabaseIdentifier,
+            **kwargs,
+        )
+
+    def dbid(self) -> Identifier:
+        if self.id is None:
+            raise ValueError("Model not tracked in database")
+        return self.id
--- src/api/models/role.py
+++ src/api/models/role.py
@@ -2,12 +2,12 @@ from typing import TYPE_CHECKING, Annotated
 from uuid import UUID
 
 from fastapi import Depends, HTTPException
-from sqlalchemy import BigInteger
 from sqlalchemy.exc import NoResultFound
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlmodel import Field, Relationship, SQLModel, select
 
 from ..db import SessionDep
+from ._util import Model
 from .organization import Organization, OrganizationDep
 
 if TYPE_CHECKING:
@@ -15,20 +15,14 @@ if TYPE_CHECKING:
 
 
 class RoleUserLink(AsyncAttrs, SQLModel, table=True):
-    role_id: int | None = Field(default=None, foreign_key="role.id", primary_key=True, sa_type=BigInteger)
+    role_id: int | None = Model.foreign_key_field("role", nullable=True, primary_key=True)
     user_id: UUID = Field(foreign_key="user.id", primary_key=True)
 
 
-class Role(AsyncAttrs, SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True, sa_type=BigInteger)
-    organization_id: int | None = Field(default=None, foreign_key="organization.id")
+class Role(AsyncAttrs, Model, table=True):
+    organization_id: int | None = Model.foreign_key_field("organization", nullable=True)
     organization: Organization | None = Relationship(back_populates="roles")
     users: list["User"] = Relationship(back_populates="roles", link_model=RoleUserLink)
-
-    def dbid(self) -> int:
-        if self.id is None:
-            raise ValueError("Model not tracked in database")
-        return self.id
 
 
 async def _lookup(session: SessionDep, organization: OrganizationDep, role_id: int) -> Role:
