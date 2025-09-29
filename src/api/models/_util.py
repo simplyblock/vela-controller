22b7e271b89469e80498f8d8b1062c28f54ff235
Max Schettler <max@simplyblock.io>
2025-09-29 15:34:36 +0200

    Remove slug generation

--- src/api/models/_util.py
+++ src/api/models/_util.py
@@ -1,31 +1,18 @@
 from typing import Annotated
 
-from pydantic import AfterValidator, StringConstraints
 from pydantic import Field as PDField
-from slugify import slugify
+from pydantic import StringConstraints
 from sqlalchemy import BigInteger
 from sqlmodel import Field as SQLField
 from sqlmodel import SQLModel
 
 from ..._util import dbstr
 
-
-def update_slug(mapper, connection, target):  # noqa
-    target.slug = slugify(target.name, max_length=50)
-
-
-def _validate_sluggable(string: str):
-    if len(slugify(string, max_length=50)) == 0:
-        raise ValueError("Derived slug is empty")
-    return string
-
-
 Name = Annotated[
     dbstr,
     StringConstraints(
         min_length=1,
     ),
-    AfterValidator(_validate_sluggable),
 ]
 
 
