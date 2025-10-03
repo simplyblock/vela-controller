302f06f7397439afd8d5f5004cf8ea8359c97aa5
Max Schettler <max.schettler@posteo.de>
2025-10-03 13:08:07 +0200

    Remove defaults for required values

--- src/deployment/settings.py
+++ src/deployment/settings.py
@@ -7,10 +7,10 @@ class Settings(BaseSettings):
     deployment_namespace_prefix: str = "vela"
     deployment_release_name: str = "supabase"
     deployment_host: str = "localhost"
-    cloudflare_api_token: str = ""
-    cloudflare_zone_id: str = ""
-    cloudflare_branch_ref_cname: str = ""
-    cloudflare_domain_suffix: str = ""
+    cloudflare_api_token: str
+    cloudflare_zone_id: str
+    cloudflare_branch_ref_cname: str
+    cloudflare_domain_suffix: str
 
 
 settings = Settings()  # type: ignore[call-arg]
