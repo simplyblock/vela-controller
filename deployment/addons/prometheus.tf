b7501da82171ec9cccaa8e91f450079538f9bb41
Geoffrey Israel <israelgeoffrey13@gmail.com>
2025-10-09 10:39:59 +0100

    added function to patch grafana k8s mgmt deployment (#32)
    * added function to patch grafana k8s mgmt deployment
    
    * patch the volume to include key pem
    
    * patch grafana mgmt service deployment
    
    * use patch type strategic
    
    * add nginx reverse proxy sidecar container for tls
    
    * removed env insecure tls
    
    * use separate grafana for vela
    
    * added prometheus chart
    
    * updated chart README
    
    * added vela grafana service
    
    * added vela grafana service
    
    * fix grammar
    
    * added https protocol keycloak
    
    * removed nginx sidecar configmap
    
    * removed cluster issuer from chart
    
    * install prometheus helm chart via terraform
    
    * updated vela HTTPRoute port
    
    * removed chart README.md
    
    * Update prometheus.tf
    
    * Update certificate.yaml
    
    * Update auth.yaml
    
    * updated controller VELA_JWT_SECRET port to 8443
    
    * updated controller VELA_JWT_SECRET port to 8443
    
    * made prometheus datasource url configurable

---
+++ deployment/addons/prometheus.tf
@@ -0,0 +1,37 @@
+resource "helm_release" "prometheus" {
+  name             = "prometheus"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "prometheus"
+  namespace        = "monitoring"
+  create_namespace = true
+  version          = "25.18.0"
+
+  set {
+    name  = "server.fullnameOverride"
+    value = "vela-prometheus"
+  }
+
+  set {
+    name  = "server.replicaCount"
+    value = 1
+  }
+
+  set {
+    name  = "server.statefulSet.enabled"
+    value = true
+  }
+
+  set {
+    name  = "server.persistentVolume.enabled"
+    value = true
+  }
+
+  set {
+    name  = "server.persistentVolume.size"
+    value = "5Gi"
+  }
+  set {
+    name  = "server.service.servicePort"
+    value = 9090
+  }
+}
