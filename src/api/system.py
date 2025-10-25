b64bf49c403379b32c549fdded91018697a54cf7
Christoph Engelbert (noctarius) <me@noctarius.com>
2025-10-25 16:08:17 +0200

    Added system data for resource limits to be used in the frontend

--- src/api/system.py
+++ src/api/system.py
@@ -1,8 +1,26 @@
 from fastapi import APIRouter, Depends
 from sqlmodel import select
 
+from .._util import (
+    DB_SIZE_MAX,
+    DB_SIZE_MIN,
+    DB_SIZE_STEP,
+    IOPS_MAX,
+    IOPS_MIN,
+    IOPS_STEP,
+    MEMORY_MAX,
+    MEMORY_MIN,
+    MEMORY_STEP,
+    STORAGE_SIZE_MAX,
+    STORAGE_SIZE_MIN,
+    STORAGE_SIZE_STEP,
+    VCPU_MILLIS_MAX,
+    VCPU_MILLIS_MIN,
+    VCPU_MILLIS_STEP,
+)
 from .auth import authenticated_user
 from .db import SessionDep
+from .models.resources import ResourceLimitDefinitionPublic
 from .models.role import AccessRight
 
 api = APIRouter(dependencies=[Depends(authenticated_user)], tags=["system"])
@@ -19,3 +37,22 @@ async def list_available_permissions(
     result = await session.execute(stmt)
     entries = [row[0] for row in result.all()]
     return entries
+
+
+@api.get("/resource-limit-definitions")
+async def list_resource_limit_definitions() -> list[ResourceLimitDefinitionPublic]:
+    return [
+        ResourceLimitDefinitionPublic(
+            resource_type="milli_vcpu", min=VCPU_MILLIS_MIN, max=VCPU_MILLIS_MAX, step=VCPU_MILLIS_STEP, unit="Millis"
+        ),
+        ResourceLimitDefinitionPublic(
+            resource_type="ram", min=MEMORY_MIN, max=MEMORY_MAX, step=MEMORY_STEP, unit="MiB"
+        ),
+        ResourceLimitDefinitionPublic(resource_type="iops", min=IOPS_MIN, max=IOPS_MAX, step=IOPS_STEP, unit="IOPS"),
+        ResourceLimitDefinitionPublic(
+            resource_type="database_size", min=DB_SIZE_MIN, max=DB_SIZE_MAX, step=DB_SIZE_STEP, unit="GB"
+        ),
+        ResourceLimitDefinitionPublic(
+            resource_type="storage_size", min=STORAGE_SIZE_MIN, max=STORAGE_SIZE_MAX, step=STORAGE_SIZE_STEP, unit="GB"
+        ),
+    ]
