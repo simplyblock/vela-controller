a702e55131cfaf673a57051a3158c428b915b708
Max Schettler <max@simplyblock.io>
2025-11-05 16:34:31 +0100

    Introduce database migrations

---
+++ scripts/alembic.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from subprocess import run
+
+from testcontainers.postgres import PostgresContainer
+
+
+def _run_alembic(args: list[str], postgres, **kwargs):
+    driver = "psycopg2"
+    return run(
+        ["alembic"] + args,
+        env={
+            **os.environ,
+            "VELA_POSTGRES_URL": postgres.get_connection_url().replace("postgresql://", f"postgresql+{driver}://"),
+        },
+        **kwargs,
+    )
+
+
+def main():
+    # Work around tox passing `posargs` as single argument
+    args = sys.argv[1:]
+    if len(args) == 1:
+        args = args[0].split(" ")
+
+    with PostgresContainer("postgres:17") as postgres:
+        _run_alembic(["upgrade", "head"], postgres, check=True)
+        return _run_alembic(args, postgres).returncode
+
+
+if __name__ == "__main__":
+    sys.exit(main())
