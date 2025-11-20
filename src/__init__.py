6d1fcfa47105e8e7c4a576de81503848961be014
Manohar Reddy <manohar@simplyblock.io>
2025-11-20 15:23:31 +0100

    control log level via env variable

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,10 @@
 import logging
+import os
 from typing import Final
 
 _LOG_FORMAT: Final[str] = "%(asctime)s %(levelname)s %(name)s: %(message)s"
 _DATE_FORMAT: Final[str] = "%Y-%m-%d %H:%M:%S"
+_LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
 
 
 def _configure_logging() -> None:
@@ -10,7 +12,9 @@ def _configure_logging() -> None:
     if root_logger.handlers:
         return
 
-    logging.basicConfig(level=logging.INFO, format=_LOG_FORMAT, datefmt=_DATE_FORMAT)
+    logging.basicConfig(
+        level=logging._nameToLevel.get(_LOG_LEVEL, logging.INFO), format=_LOG_FORMAT, datefmt=_DATE_FORMAT
+    )
 
 
 _configure_logging()
