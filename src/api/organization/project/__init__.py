3b6b444f2ef79d60903ce3f6b6463acb0ac234d3
noctarius aka Christoph Engelbert <me@noctarius.com>
2025-11-27 17:02:20 +0100

    Removed all exchanged or unused kubevirt features (#315)

--- src/api/organization/project/__init__.py
+++ src/api/organization/project/__init__.py
@@ -10,8 +10,7 @@ from sqlalchemy.exc import IntegrityError
 from sqlmodel import select
 
 from ...._util import Identifier
-from ....deployment import delete_deployment, get_autoscaler_vm_identity, get_db_vmi_identity
-from ....deployment.kubernetes.kubevirt import call_kubevirt_subresource
+from ....deployment import delete_deployment, get_autoscaler_vm_identity
 from ....deployment.kubernetes.neonvm import set_virtualmachine_power_state
 from ....exceptions import VelaKubernetesError
 from ....models.organization import Organization
@@ -507,14 +506,7 @@ async def suspend(session: SessionDep, _organization: OrganizationDep, project: 
     errors = []
 
     for branch in branches:
-        namespace, vmi_name = get_db_vmi_identity(branch.id)
         autoscaler_namespace, autoscaler_vm_name = get_autoscaler_vm_identity(branch.id)
-        try:
-            # a paused VM will still consume resources, so we stop it instead
-            # https://kubevirt.io/user-guide/user_workloads/lifecycle/#pausing-and-unpausing-a-virtual-machine
-            await call_kubevirt_subresource(namespace, vmi_name, "stop")
-        except ApiException as e:
-            errors.append(f"{vmi_name}: {e.status}")
         try:
             await set_virtualmachine_power_state(autoscaler_namespace, autoscaler_vm_name, "Stopped")
         except ApiException as e:
@@ -547,12 +539,7 @@ async def resume(session: SessionDep, _organization: OrganizationDep, project: P
     errors = []
 
     for branch in branches:
-        namespace, vmi_name = get_db_vmi_identity(branch.id)
         autoscaler_namespace, autoscaler_vm_name = get_autoscaler_vm_identity(branch.id)
-        try:
-            await call_kubevirt_subresource(namespace, vmi_name, "start")
-        except ApiException as e:
-            errors.append(f"{vmi_name}: {e.status}")
         try:
             await set_virtualmachine_power_state(autoscaler_namespace, autoscaler_vm_name, "Running")
         except ApiException as e:
