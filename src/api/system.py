492d246b2070f6e7c70d0936963e5a8b1cdcf77a
Christoph Engelbert (noctarius) <me@noctarius.com>
2026-01-30 08:42:20 +0100

    Fixed system resource limits which used the hardcoded information, not the database ones

--- src/api/system.py
+++ src/api/system.py
@@ -21,8 +21,9 @@ from .._util import (
     VCPU_MILLIS_MIN,
     VCPU_MILLIS_STEP,
 )
-from ..models.resources import ResourceLimitDefinitionPublic
+from ..models.resources import ResourceLimitDefinitionPublic, ResourceType
 from ..models.role import AccessRight
+from ._util.resourcelimit import get_system_resource_limits
 from .auth import authenticated_user
 from .db import SessionDep
 
@@ -64,20 +65,37 @@ async def list_available_permissions(
 
 
 @api.get("/resource-limit-definitions", dependencies=[Depends(authenticated_user)])
-async def list_resource_limit_definitions() -> list[ResourceLimitDefinitionPublic]:
+async def list_resource_limit_definitions(
+    session: SessionDep,
+) -> list[ResourceLimitDefinitionPublic]:
+    system_limits = await get_system_resource_limits(session)
+
+    def _get_limit(resource_type: ResourceType, default: int) -> int:
+        return system_limits[resource_type].max_total if system_limits[resource_type] else default
+
+    max_vcpu_millis = _get_limit(ResourceType.milli_vcpu, VCPU_MILLIS_MAX)
+    max_ram_bytes = _get_limit(ResourceType.ram, MEMORY_MAX)
+    max_iops = _get_limit(ResourceType.iops, IOPS_MAX)
+    max_database_size_bytes = _get_limit(ResourceType.database_size, DB_SIZE_MAX)
+    max_storage_size_bytes = _get_limit(ResourceType.storage_size, STORAGE_SIZE_MAX)
+
     return [
         ResourceLimitDefinitionPublic(
-            resource_type="milli_vcpu", min=VCPU_MILLIS_MIN, max=VCPU_MILLIS_MAX, step=VCPU_MILLIS_STEP, unit="Millis"
+            resource_type="milli_vcpu", min=VCPU_MILLIS_MIN, max=max_vcpu_millis, step=VCPU_MILLIS_STEP, unit="Millis"
         ),
         ResourceLimitDefinitionPublic(
-            resource_type="ram", min=MEMORY_MIN, max=MEMORY_MAX, step=MEMORY_STEP, unit="MiB"
+            resource_type="ram", min=MEMORY_MIN, max=max_ram_bytes, step=MEMORY_STEP, unit="MiB"
         ),
-        ResourceLimitDefinitionPublic(resource_type="iops", min=IOPS_MIN, max=IOPS_MAX, step=IOPS_STEP, unit="IOPS"),
+        ResourceLimitDefinitionPublic(resource_type="iops", min=IOPS_MIN, max=max_iops, step=IOPS_STEP, unit="IOPS"),
         ResourceLimitDefinitionPublic(
-            resource_type="database_size", min=DB_SIZE_MIN, max=DB_SIZE_MAX, step=DB_SIZE_STEP, unit="GB"
+            resource_type="database_size", min=DB_SIZE_MIN, max=max_database_size_bytes, step=DB_SIZE_STEP, unit="GB"
         ),
         ResourceLimitDefinitionPublic(
-            resource_type="storage_size", min=STORAGE_SIZE_MIN, max=STORAGE_SIZE_MAX, step=STORAGE_SIZE_STEP, unit="GB"
+            resource_type="storage_size",
+            min=STORAGE_SIZE_MIN,
+            max=max_storage_size_bytes,
+            step=STORAGE_SIZE_STEP,
+            unit="GB",
         ),
     ]
 
