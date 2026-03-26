53dfb1e477bc7aea2b050ea08afa2465516b8c90
Manohar Reddy <manohar@simplyblock.io>
2026-03-26 18:43:23 +0530

    tests: add tests for branch start/stop

---
+++ tests/branches/test_start_stop.py
@@ -0,0 +1,41 @@
+import pytest
+from conftest import BRANCH_TIMEOUT_SEC, wait_for_status
+
+pytestmark = pytest.mark.branch
+
+
+@pytest.fixture(scope="module")
+def org(make_org):
+    return make_org("test-org-start-stop")
+
+
+@pytest.fixture(scope="module")
+def project(make_project, org):
+    return make_project(org, "test-project-start-stop")
+
+
+@pytest.fixture(scope="module")
+def branch_id(make_branch, org, project):
+    return make_branch(org, project, "test-branch-start-stop")
+
+
+def test_branch_stop(client, org, project, branch_id):
+    r = client.post(f"organizations/{org}/projects/{project}/branches/{branch_id}/stop")
+    assert r.status_code == 204
+    wait_for_status(
+        client,
+        f"organizations/{org}/projects/{project}/branches/{branch_id}/",
+        "STOPPED",
+        BRANCH_TIMEOUT_SEC,
+    )
+
+
+def test_branch_start(client, org, project, branch_id):
+    r = client.post(f"organizations/{org}/projects/{project}/branches/{branch_id}/start")
+    assert r.status_code == 204
+    wait_for_status(
+        client,
+        f"organizations/{org}/projects/{project}/branches/{branch_id}/",
+        "ACTIVE_HEALTHY",
+        BRANCH_TIMEOUT_SEC,
+    )
