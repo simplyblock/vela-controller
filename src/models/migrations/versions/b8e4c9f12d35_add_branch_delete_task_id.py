ac636e00fd5a91e7e07efaf6dd0d4c0da90f9a14
Max Schettler <max@simplyblock.io>
2026-04-01 13:58:04 +0200

    fixup! Convert branch deletion to celery job

--- src/models/migrations/versions/b8e4c9f12d35_add_branch_delete_task_id.py
+++ src/models/migrations/versions/b8e4c9f12d35_add_branch_delete_task_id.py
@@ -1,8 +1,8 @@
 """Add branch delete_task_id
 
 Revision ID: b8e4c9f12d35
-Revises: a1b2c3d4e5f6
-Create Date: 2026-04-01 00:00:00.000000
+Revises: 2b4e8f1a6c03
+Create Date: 2026-04-01 13:57:00.000000
 
 """
 from typing import Sequence, Union
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'b8e4c9f12d35'
-down_revision: Union[str, Sequence[str], None] = 'a1b2c3d4e5f6'
+down_revision: Union[str, Sequence[str], None] = '2b4e8f1a6c03'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
