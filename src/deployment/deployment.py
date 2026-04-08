538f1ea3e2c5e33bee4bccfd92e6d7a1fc228696
Max Schettler <max@simplyblock.io>
2026-04-08 15:18:11 +0200

    Bump vela-os version

--- src/deployment/deployment.py
+++ src/deployment/deployment.py
@@ -18,7 +18,7 @@ _SUPPORTED_DATABASE_IMAGE_TAG = {
     },
     "18.1-velaos": {
         "image": "docker.io/simplyblock/vela-image",
-        "tag": "sha-51e562f",
+        "tag": "sha-842bab7",
     },
 }
 
